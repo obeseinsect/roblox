5fd579695d854363d650330982dfece861150fb1
obeseinsect <75360517+obeseinsect@users.noreply.github.com>
2021-06-15 13:39:34 -0500

    Update AFS.lua

--- Games/Anime Fighters Sim/AFS.lua
+++ Games/Anime Fighters Sim/AFS.lua
@@ -45,7 +45,7 @@ workspace.Effects.ChildAdded:Connect(function(penis)
 end)
 
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
-local window = Material.Load({Title = "Anime Fighters Simulator - cameron was here", Style = 1, SizeX = 500, SizeY = 350, Theme = "Aqua"})
+local window = Material.Load({Title = "Anime Fighters Simulator - cameron was here", Style = 3, SizeX = 500, SizeY = 350, Theme = "Aqua"})
 local tab = window.New({Title = "Main"})
 tab.Toggle({Text = "Enabled", Callback = function(v) farm = v end})
 tab.Dropdown({ Text = "Selection", Callback = function(v) selection = v end, Options = enemies})
